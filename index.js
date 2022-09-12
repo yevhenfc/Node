e33510208c62b3bd3b7beaf5237b1b52ce308972
yevhenfc <yevhenfc@gmail.com>
2022-09-12 21:39:16 +0300

    export+require

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('adasdadas');
+// const superSum = require('./math');
+// const {superSum, pow2} = require('./math');
+const Math = require('./math');
+const fs = require('fs');
+
+const res = Math.superSum(1,2,3,4,5);
+const data = fs.readFileSync('./README.md', {encoding: 'utf8'});
+
+console.log(res);
+console.log(Math.superSum(6,7,8,9,10));
+console.log(Math.pow2(3));
+console.log(data);
---
+++ math.js
@@ -0,0 +1,9 @@
+// const superSum = (...args) => args.reduce((total, i) => total + i, 0);
+// const pow2 = (val) => val ** 2;
+module.exports.superSum = (...args) => args.reduce((total, i) => total + i, 0);
+module.exports.pow2 = (val) => val ** 2;
+
+
+
+// module.exports = superSum;
+// module.exports = {superSum, pow2};
