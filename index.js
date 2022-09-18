bcbb2ec3d010d32ac23aa2be1dd57a63ee212b2f
yevhenfc <yevhenfc@gmail.com>
2022-09-18 06:51:57 +0300

    requestListiner fix

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const http = require ('http');
-const PORT = 5000;
-const HOST = '127.0.0.1';
-const requestListiner = require('./requestListener');
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+const HOST = '127.0.0.1'; // 'localhost'
+const requestListener = require('./requestListener');
 
-const server = http.createServer((req, res) => {
-    const {url, method} = req;
-    console.log('url:', url);   
-    console.log('method:', method);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    const indexContent = fs.readFileSync('./index.html', "utf8");
-    console.log(indexContent);
-    res.end(indexContent);
-}); 
+const server = http.createServer(requestListener);
 
-server.listen(PORT, HOST, () => {console.log(`Server is listening ${HOST} on ${PORT} port`)});
+server.listen(PORT, HOST, () => {console.log(`Server is listening ${HOST} on ${PORT} port!`)})
--- requestListener.js
+++ requestListener.js
@@ -4,41 +4,41 @@ const path = require('path');
 
 const readFile = util.promisify(fs.readFile);
 
-module.exports = function requestListiner(req, res){
+const requestListener = (req, res) =>{
     const {url, method} = req;
-    const page = url == '/' ? 'index.html' : url;
-    const regPage = /^.*\.html$/;
-    if (regPage.test(page)){
-      const pagePath = path.join(__dirname, '/pages/', page);
-      if (fs.existsSync(pagePath, {encoding: 'utf-8'})){
-        readFile(pagePath)
-        .then(data => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type','text/html');
-        })
-        .catch(err => {
-          res.statusCode = 500;
-          res.end('Server error')
-        });}
-      else {
-        res.statusCode = 404;
-        res.end('Page is not found')
-      }
-    }
-    switch (method) {
-      case 'GET': {
-        
-        break;  
-      };
-        
-        break;
     
+    switch (method) {
+      case "GET": {
+        const page = url == "/" ? "index.html" : url;
+        const regPage = /^.*\.html$/;
+        if (regPage.test(page)) {
+          const pagePath = path.join(__dirname, "/pages/", page);
+          if (fs.existsSync(pagePath, { encoding: "utf-8" })) {
+            readFile(pagePath)
+              .then((data) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "text/html");
+                res.end(data);
+              })
+              .catch((err) => {
+                res.statusCode = 500;
+                res.end("Server error");
+              });
+          } else {
+            res.statusCode = 404;
+            res.end("Page not found");
+          }
+        } else{
+          res.statusCode = 403;
+          res.end("Resource is not html");
+        }
+        break;
+      }
       default: {
         res.statusCode = 400;
-        res.end('Bed request');
-        break
+        res.end("Bad request ((( ");
       }
     }
+};
 
-    // res.end(`Hello! Method is ${method} in url ${url}`);
-  };
+module.exports = requestListener;
